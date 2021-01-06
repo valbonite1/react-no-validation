47df1765c4623622328d30cf123fc5fb9412e89b
Val Bonite <valbonite@gmail.com>
2021-01-06 18:41:13 +0800

    added edit function. revised code. nawala comments sorry hehe

--- src/components/Pages/CommentBox/CommentItem.js
+++ src/components/Pages/CommentBox/CommentItem.js
@@ -1,54 +1,40 @@
 /* ITO YUNG COMPONENT WHERE WE WILL RENDER THE DATA THAT WE GET FROM THE AddModal.js Component */
 
-import React, { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid'; //this is another library. please install this. explanation in NotesForJatt.
-import AddModal from './AddModal';
-import initialState from './InitialState'; //nilabas ko sa ibang file yung data para hindi magulo sa loob ng code. This file is not a component, it just contains data. This is the initial data that you can provide. Gusto ko kasi parang may laman na na fake comments. HAHAHAHA yun yung screenshot na sinend ko sayo. You don't have to do this if you dont want to. You can simply start with an empty array and add comments from there.
+import React, {useState} from 'react';
 
-const CommentItem = () => {
+ 
+const CommentItem = ( { deleteRecom, editRow, recommendations } ) => {
 
-  //this is the useEffect that will get our initial data. 
+   /* ========================HANDLES MODAL FUNCTION======================== */
 
-  useEffect(()=>{
+  // What is a modal? Modal yun yung kapag may priness kang button may lalabas na parang body 
 
-    const data = localStorage.getItem('recommendations')
-    
-    if(data){ //the if condition will ask if data exists, if it exists, then it will parse it.
-      setRecommendations(JSON.parse(data))
-     }
+  // Ito yung set of functions na maghahandle sa pag open-close ng modal natin 
 
-    },[]) //this dependancy will just run everytime the page reloads. so once lang siya ever magrrun. 
+  const [modalOpen, setModalOpen] = useState(false); //We initialize the state of the modal which is false. meaning kapag false. nakaclose siya
 
-  const [recommendations, setRecommendations] = useState(initialState) //dito mo iinitialize ang initial data. So kumbaga, recommendations will have an initial data na nasa loob ng initialState. You can start with an empty array. So you just start with useState([]);
+  // handleModalOpen will open the modal. nakalink to siya sa button na may fas fa-pen na name 
+
+  const handleModalOpen = () => {
+    setModalOpen(!false);
+  }
+
+  // this will just close the modal. nilink ko to siya sa Cancel button. 
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  }
+
+  // style lang to ng kulay ng backgorund color sa likod ng modal. so parang magffade yung likod and magddark and magkakaroon ng focus sa modal mismo 
   
-  //the addRecom function yung responsible ng pag-add ng new data. 
-  const addRecom = (firstName, lastName, comment) => {
 
-    //yung nangyayari dito is spread operator. kukunin niya yung current existing data isspread operator niya as ...recommendations, then iaadd niya yung new data which is the {id, firstName, lastName, comment}. Ito yung ginagawa mo if you want to add new data to an array. Kapag hindi mo kasi ginawa yung spread operator na ...recommendations, it will just replace the old data with the new data. so yung ginagawa dito is iccopy niya yung old data through ...recommendations, then dugtong yung new data.
 
-    setRecommendations([...recommendations, { id: uuidv4(), firstName, lastName, comment}])
-  }
-
-  //ito yung delete function. dinedelete niya yung entry na pipiliin mo.
-
-  const deleteRecom = (index) => {
-    let newRecom = recommendations;
-    newRecom.splice(index, 1);
-    setRecommendations([...newRecom]);
-  }
-
-  //ito yung useEffect na magsset ng data sa localStorage. so everytime something changes sa recommendations data mo (as seen in the dependency), it will run and set the new change sa localStorage. 
-
-  useEffect(() => {
-    let inputData = JSON.stringify(recommendations);
-    localStorage.setItem('recommendations', inputData);
-  }, [recommendations])
+  /* ================================================ */
 
   return(
     <>
-      <AddModal addRecom={addRecom}/>
       <ul className='comment-list'>
-        {recommendations.map( (recom, index) => {
+        {recommendations.map((recom, index) => {
           return(
             <li key={recom.id}>
               <div className='comment-icon'>
@@ -63,7 +49,7 @@ const CommentItem = () => {
                   <p>{recom.comment}</p>
                 </div>
                 <div className='comment-btn'>
-                  <button className='btn-edit'><i className='far fa-edit'></i></button>
+                  <button className='btn-edit' onClick={() => editRow(recom)}><i className='far fa-edit'></i></button>
                   <button onClick={() => deleteRecom(index)} className='btn-trash'><i className='far fa-trash-alt'></i></button>
                 </div>
               </div>
---
+++ src/components/Pages/CommentBox/EditComment.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import Modal from 'react-modal';
+
+const EditComment = ( props ) => {
+
+  const [user, setUser] = useState(props.currentUser);
+
+  useEffect(
+    () => {
+      setUser(props.currentUser);
+    },
+    [props]
+  );
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      props.updateUser(user.id, user);
+      
+    }
+
+  return(
+    <>
+    <div className='modal-container'>
+        <div className='modal-header'>
+          <h2>What changes do you want to make?</h2>
+          <p>Update your comment here!</p>
+        </div>
+        <div className='form-container' autoComplete='off'>
+            <div className='input-container'>
+              <label>First Name</label>
+              <input type='text' required value={user.firstName} name='firstName' onChange={handleInputChange}/>
+            </div>
+            <div className='input-container'>
+              <label>Last Name</label>
+              <input type='text' required value={user.lastName} name='lastName' onChange={handleInputChange}/>
+            </div>
+            <div className='input-container'>
+              <label>Recommendation</label>
+              <textarea name='Comment' rows='10' cols='40' required value={user.comment} name='comment' onChange={handleInputChange} />
+            </div>
+        </div>
+        <div className='modal-btn'>
+          <button className='btn-write' onClick={handleSubmit}>Update</button>
+          <button className='btn-cancel' onClick={() => props.setEditing(false)}>Cancel</button>
+        </div>
+    </div>
+  </>
+  );
+}
+
+export default EditComment;
